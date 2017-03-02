bd743bb9af841e38264e3759f7523266ecbb3294
root <Wey Gu>
2017-03-02 18:54:58 +0800

    name validation

--- dataMigrate.py
+++ dataMigrate.py
@@ -11,18 +11,44 @@ sys.setdefaultencoding('utf8')
 workbookInRAM = load_workbook('data/defaultData.xlsx')
 worksheetInRAM = workbookInRAM.active
 
+
+# Functions validateNameLists
+# Validation of Name lists
+def validateNameLists(strNameList):
+    outputList = []
+    outputList = re.split(',|;|/| and ',strNameList)
+    # Removing email like <for.bar@example.com>
+    for strName in outputList:
+        strName = re.split('<',strName)[0]
+        if strName.startswith(' '):
+            strName = strName[1:]
+        # print strName
+    return outputList
+
+
+
+
 # Convert description from html to plain string here:
 for cell in worksheetInRAM['Z']:
     cell.value = html2textCall(cell.value)
-# Validation of Name lists
 
-def validateNameLists(strNameList):
-    outputList = list()
-    outputList = re.split(r'(?:,|;|\s)\s*',strNameList)a
-    # Removing email like <for.bar@example.com>
-    for strName in outputList:
-        strName = strName.split("<")[0]
-    return outputList
+# filter names of Developer
+columnU = []
+for cell in worksheetInRAM['U']:
+    if cell.value == None:
+        columnU.append([])
+    else:
+        columnU.append(validateNameLists(cell.value))
 
+# filter names of InnovatorName
+columnF = []
+for cell in worksheetInRAM['F']:
+    if cell.value == None:
+        columnF.append([])
+    else :
+        columnF.append(validateNameLists(cell.value))
+
+print len(columnU)
+print columnF
 
 # Insert data
