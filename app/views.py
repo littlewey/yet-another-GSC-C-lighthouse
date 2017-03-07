0c5cadbc37e12f388b49ea126fab7b5ff113f950
Wey Gu <littlewey@gmail.com>
2017-03-07 11:39:51 +0800

    search with tab/serviceArea

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
-from flask import render_template, g, redirect, url_for, request
+from flask import render_template, g, redirect, url_for
 from app import app
-from app import db, models
+from app import models
 from config import TOOLS_PER_PAGE
 from forms import SearchForm
 from config import MAX_SEARCH_RESULTS, SiteNameString, tabs
@@ -10,38 +10,47 @@ from config import MAX_SEARCH_RESULTS, SiteNameString, tabs
 def search():
     search_form = SearchForm()
     g.search_form = search_form
-    #print type(g.search_form.search.data)
-    return redirect(url_for('search_results', query = g.search_form.search.data , page = 1 ))
+    #print g.search_form.search.data
+    return redirect(url_for('search_results', query = g.search_form.search.data , page = 1,serviceArea = 'all_serviceArea' ))
 
-@app.route('/search_results/<query>/<int:page>')
-def search_results(query, page = 1):
-    pagination = models.Tool.query.whoosh_search(query, MAX_SEARCH_RESULTS, or_=True).order_by(models.Tool.DownloadTimes.desc()).paginate(page, per_page=TOOLS_PER_PAGE, error_out = False)
+@app.route('/search_results/<serviceArea>/<query>/<int:page>')
+def search_results(query,serviceArea='all_serviceArea', page = 1):
+    print query
+    if serviceArea.startswith('all'):
+        pagination = models.Tool.query.whoosh_search(query, MAX_SEARCH_RESULTS, or_=True).order_by(models.Tool.DownloadTimes.desc()).paginate(page, per_page=TOOLS_PER_PAGE, error_out = False)
+    else:
+        pagination = models.Tool.query.filter_by(ServiceArea=serviceArea).whoosh_search(query, MAX_SEARCH_RESULTS, or_=True).order_by(models.Tool.DownloadTimes.desc()).paginate(page, per_page=TOOLS_PER_PAGE, error_out = False)
+
     #print pagination
     results = pagination.items
     #print results
-    return render_template('search_results.html',
+    return render_template('index.html',
         query = query,
         title=query + " search results",
         toolScope = results,
         pagination = pagination,
         tabs = tabs,
         endpoint='search_results',
+        serviceArea=serviceArea,
         SiteNameString=SiteNameString
         )
 
 @app.route('/', methods = ['GET', 'POST'])
 @app.route('/tool', methods = ['GET', 'POST'])
-@app.route('/tool/<query>/<int:page>', methods = ['GET', 'POST'])
-def tool(page = 1, query = 'all', SiteNameString=SiteNameString):
-    pagination = models.Tool.query.order_by(models.Tool.DownloadTimes.desc()).paginate(page, per_page=TOOLS_PER_PAGE, error_out = False)
+@app.route('/tool/<serviceArea>/<query>/<int:page>', methods = ['GET', 'POST'])
+def tool(page = 1, serviceArea= 'all_serviceArea',query='all' ,SiteNameString=SiteNameString):
+    if serviceArea.startswith('all'):
+        pagination = models.Tool.query.order_by(models.Tool.DownloadTimes.desc()).paginate(page, per_page=TOOLS_PER_PAGE, error_out = False)
+    else:
+        pagination = models.Tool.query.filter_by(ServiceArea=serviceArea).order_by(models.Tool.DownloadTimes.desc()).paginate(page, per_page=TOOLS_PER_PAGE, error_out = False)
     tools = pagination.items
     #print tools
-    return render_template("tool.html",
+    return render_template("index.html",
                             toolScope=tools,
                             pagination=pagination,
                             tabs=tabs,
                             endpoint='tool',
-                            query = 'all',
+                            serviceArea= serviceArea,
                             SiteNameString=SiteNameString
                             )
 
